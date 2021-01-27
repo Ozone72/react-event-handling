01fa943d24798b7f9bc16b872f6d2a0478cf17f9
Orin Fletcher <orin.fletcher@gmail.com>
2021-01-27 15:54:20 -0800

    adds mouseOver logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingTxt, setheadingTxt] = useState("Hello");
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setheadingTxt("Submitted");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{headingTxt}</h1>
+      <label htmlFor="someName"></label>
+      <input type="text" placeholder="What's your name?" id="someName" />
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
